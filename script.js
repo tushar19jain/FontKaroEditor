a8f5f8a8fc63ac57bfa2eb6ff1ab99a3afd4e88b
Tushar Jain <112325029+tushar19jain@users.noreply.github.com>
2022-12-16 20:04:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+alert("Please use in Computer or Desktop Mode. If using computer click OK.")
+function cursivechange(){    
+    document.getElementById("userInput").style.fontFamily="cursive";
+}
+function monoChange(){
+    document.getElementById("userInput").style.fontFamily="monospace";
+}
+function fantasyChange(){
+    document.getElementById("userInput").style.fontFamily="fantasy";
+}
+function arialChange(){
+    document.getElementById("userInput").style.fontFamily="arail";
+}
+function verdanaChange(){
+    document.getElementById("userInput").style.fontFamily="Verdana";
+}
+function tahomaChange(){
+    document.getElementById("userInput").style.fontFamily="tahoma";
+}
+function trebuchetChange(){
+    document.getElementById("userInput").style.fontFamily="'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif";
+}
+function timesnewromanChange(){
+    document.getElementById("userInput").style.fontFamily="'Times New Roman', Times, serif";
+}
+function georgiaChange(){
+    document.getElementById("userInput").style.fontFamily="Georgia, 'Times New Roman', Times, serif";
+}
+function courierChange(){
+    document.getElementById=("userInput").style.fontFamily="'Courier New', Courier, monospace";
+}
+function brushChange(){
+    document.getElementById("userInput").style.fontFamily="system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
+}
+function fgmChange(){
+    document.getElementById('userInput').style.fontFamily='Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+function impactChange(){
+    document.getElementById('userInput').style.fontFamily='Impact, Haettenschweiler', 'Arial Narrow Bold', sans-serif;
+}
+function georgiaChange(){
+    document.getElementById('userInput').style.fontFamily='Georgia,Times New Roman', 'Times, serif';
+}
+function lucidChange(){
+    document.getElementById('userInput').style.fontFamily='Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+function segoeChange(){
+    document.getElementById('userInput').style.fontFamily='Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
